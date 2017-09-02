a91e750b3ef25dcb8935bd2d9cf680768535df77
apdjz <14apdjz@gmail.com>
2017-09-02 10:20:33 -0400

    Update guess.js

--- guess.js
+++ guess.js
@@ -1,59 +1,59 @@
-window.onload = function (){
-startGame();
+window.onload = function() {
+	startGame();
 };
-function makeGuess(){
-	var guess =  parseInt(document.getElementById("num").value);
+
+function makeGuess() {
+	var guess = parseInt(document.getElementById("num").value);
 	game.prototype.playersGuessSubmission(guess);
 }
-var game = function(){
-	this.pGuess = null;
-	this.rand = randomNumberGenerator();
-	this.last=[];
-
+var pGuess = null;
+var rand = randomNumberGenerator();
+var game = function() {
+	window.last = [];
 }
 
-function randomNumberGenerator(){
- return Math.floor((Math.random() * 100) + 1);
+function randomNumberGenerator() {
+	return Math.floor((Math.random() * 100) + 1);
 }
 
-function startGame(){
+function startGame() {
 	return new game();
 }
-
-game.prototype.closeness = function(){
-	return Math.abs(pGuess-rand);
+game.prototype.closeness = function() {
+	return Math.abs(pGuess - rand);
 }
-
 game.prototype.isLower = function() {
-    return this.pGuess < this.rand;
+	return pGuess < rand;
 }
-
+game.prototype.playersGuessSubmission = function(guess) {
+	if (typeof guess !== 'number' || guess < 1 || guess > 100) {
+		throw "That is an invalid guess.";
+	}
+	pGuess = guess;
+	return this.checkGuess();
+}
 game.prototype.checkGuess = function() {
-    if(this.pGuess == this.rand) {
-        document.getElementById("status").innerHTML = "You Win";
-    }
-    else {
-        if(this.last.indexOf(this.pGuess) > -1) {
-            return 'You have already guessed that number.';
-        }
-        else {
-            this.last.push(this.pGuess);
-            if(this.last.length === 5) {
-                document.getElementById("status").innerHTML = "You Lose";
-            }
-            else {
+	if (pGuess == rand) {
+		document.getElementById("status").innerHTML = "You Win";
+	} else {
+		if (window.last.indexOf(pGuess) > -1) { /* -1 if element not in array*/
+			return 'You have already guessed that number.';
+		} else {
+			window.last.push(pGuess);
+			if (window.last.length === 5) {
+				document.getElementById("status").innerHTML = "You Lose";
+			} else {
 				var entry = document.createElement('li');
-				entry.appendChild(document.createTextNode(this.guess));
-				unordered.appendChild(entry); 
-                var diff = this.closeness();
-                if(diff < 10){
+				entry.appendChild(document.createTextNode(pGuess));
+				unordered.appendChild(entry);
+				var diff = this.closeness();
+				if (diff < 10) {
 					document.getElementById("status").innerHTML = "Hot";
-					document.getElementById("status").style.textShadow  = '0 0 20px #fefcc9, 10px -10px 30px #feec85, -20px -20px 40px #ffae34, 20px -40px 50px #ec760c, -20px -60px 60px #cd4606, 0 -80px 70px #973716, 10px -90px 80px #451b0e';
+					document.getElementById("status").style.textShadow = '0 0 20px #fefcc9, 10px -10px 30px #feec85, -20px -20px 40px #ffae34, 20px -40px 50px #ec760c, -20px -60px 60px #cd4606, 0 -80px 70px #973716, 10px -90px 80px #451b0e';
 					document.getElementById("status").style.color = "#FFCC00";
-				}
-                else if(diff < 25) document.getElementById("status").innerHTML = "Warm";
-                else if(diff < 50) document.getElementById("status").innerHTML = "Cold";
-                else{
+				} else if (diff < 25) document.getElementById("status").innerHTML = "Warm";
+				else if (diff < 50) document.getElementById("status").innerHTML = "Cold";
+				else {
 					document.getElementById("status").innerHTML = "Ice Cold";
 					document.body.style.backgroundImage = "url('ice-background-14140151326az.jpg')";
 				}
@@ -61,31 +61,22 @@ game.prototype.checkGuess = function() {
 		}
 	}
 }
-game.prototype.playersGuessSubmission = function(guess) {
-    if(typeof guess !== 'number' || guess < 1 || guess > 100) {
-        throw "That is an invalid guess.";
-    }
-    this.pGuess = guess;
-    return this.checkGuess();
-}
 
-function hint(){
+function hint() {
 	game.prototype.provideHint();
 }
-
-
 game.prototype.provideHint = function() {
-    var hintArray = [this.rand, randomNumberGenerator(), randomNumberGenerator()];
-    return shuffle(hintArray);
+	var hintArray = [rand, randomNumberGenerator(), randomNumberGenerator()];
+	return shuffle(hintArray);
 }
 
 function shuffle(arr) { //Fisher-Yates - https://bost.ocks.org/mike/shuffle/
-   for(var i = arr.length-1; i > 0; i--) {
-       var randomIndex = Math.floor(Math.random() * (i + 1));
-       var temp = arr[i];
-       arr[i] = arr[randomIndex];
-       arr[randomIndex] = temp;
-    }
+	for (var i = arr.length - 1; i > 0; i--) {
+		var randomIndex = Math.floor(Math.random() * (i + 1));
+		var temp = arr[i];
+		arr[i] = arr[randomIndex];
+		arr[randomIndex] = temp;
+	}
 	document.getElementById("status").innerHTML = "Guessing Game";
-    return arr;
+	return arr;
 }
